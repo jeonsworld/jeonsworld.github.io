3eefd620a1dd078bbf280564c31dc09cbd4e2d14
jeonsworld <jeonsworld@gmail.com>
2020-02-06 20:37:55 +0900

    config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,7 @@ module.exports = {
       options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
         trackingId: "UA-157040981-1",
+        head: true,
       },
     },
     {
